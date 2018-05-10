14160a4e839583b82c605e40d0de577cf373b21c
Isaac Casey <isaac.casey@utoronto.mail.ca>
2018-05-10 15:50:34 -0400

    Added class variables

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,11 @@
-public class Student {
+/*
+    This is a student at a school
+ */
+
+public class Student
+{
+    private String name;
+    private String stud_id;
+    private Classroom classroom;
+
 }
