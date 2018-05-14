d629a2f7672c518bb24230ac3f6e2d4360893f98
Isaac Casey <isaac.casey@utoronto.mail.ca>
2018-05-14 14:46:50 -0400

    Basic skeleton of the class Student and Classroom

--- src/Classroom.java
+++ src/Classroom.java
@@ -1,2 +1,29 @@
-public class Classroom {
+/*
+    This is a classroom in the school
+ */
+
+import java.util.ArrayList;
+
+public class Classroom
+{
+    private String classroom_id;
+    private Teacher teacher;
+    private ArrayList<Student> students;
+
+    public Classroom(String classroom_id) {
+        this.classroom_id = classroom_id;
+        this.students = new ArrayList<>();
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public void addStudent(Student student) {
+        this.students.add(student);
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -4,32 +4,46 @@
 
 public class Student
 {
-    private String name;
+    private String f_name;
+    private String l_name;
     private String stud_id;
+    private int grade;
     private Classroom classroom;
     private Backpack backpack;
 
     //If a student does not have a backpack i.e, never been entered into the program before
-    public Student(String name, String stud_id, Classroom classroom) {
-        this.name = name;
+    public Student(String f_name, String l_name, String stud_id, int grade, Classroom classroom) {
+        this.f_name = f_name;
+        this.l_name = l_name;
         this.stud_id = stud_id;
+        this.grade = grade;
         this.classroom = classroom;
     }
 
     //If a student has a backpack i.e, has been entered into the program before
-    public Student(String name, String stud_id, Classroom classroom, Backpack backpack) {
-        this.name = name;
+    public Student(String f_name, String l_name, String stud_id, int grade, Classroom classroom, Backpack backpack) {
+        this.f_name = f_name;
+        this.l_name = l_name;
         this.stud_id = stud_id;
+        this.grade = grade;
         this.classroom = classroom;
         this.backpack = backpack;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return f_name;
+    }
+
+    public String getLastName() {
+        return l_name;
     }
 
     public String getStud_id() {
         return stud_id;
+    }
+
+    public int getGrade() {
+        return grade;
     }
 
     public Classroom getClassroom() {
