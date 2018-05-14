d29560b1d469918a4ba2bb5a20fa04dcb79cdc22
Jbattista64 <39165995+Jbattista64@users.noreply.github.com>
2018-05-14 14:30:19 -0400

    Basics of teachers

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,2 +1,41 @@
 public class Teacher {
+
+    private String fname;
+    private String lname;
+    private int grade_level;
+    private int classroom;
+
+    public Teacher(String first, String last, int grade, int ID) {
+        this.fname=first;
+        this.lname=last;
+        this.grade_level=grade;
+        this.classroom=ID;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public int getGrade_level() {
+        return grade_level;
+    }
+
+    public int getClassroom() {
+        return classroom;
+    }
+
+    public void makeBadge(){
+
+    }
+
+    public void giveBadge(Student s){
+
+    }
+    public void accessClass(){
+
+    }
 }
