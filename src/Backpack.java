14bb3edac9d1e1cb24884317cc7ccb1a3971f720
Jbattista64 <39165995+Jbattista64@users.noreply.github.com>
2018-05-14 14:57:04 -0400

    Made Backpack

--- src/Backpack.java
+++ src/Backpack.java
@@ -1,2 +1,14 @@
+
+import java.util.ArrayList;
+
+
 public class Backpack {
+
+    private ArrayList<Academic> badgeAca;
+    private ArrayList<ExtraCir> badgeExt;
+
+    public Backpack(){
+        //Creates Empty Backpack
+    }
+    
 }
