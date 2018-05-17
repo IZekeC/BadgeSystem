ef6a6c3db6c0ae44df40bf0c2f7439f763d1f15c
Jbattista64 <39165995+Jbattista64@users.noreply.github.com>
2018-05-17 14:41:50 -0400

    Badge

--- src/Badge.java
+++ src/Badge.java
@@ -1,2 +1,30 @@
 public class Badge {
+
+    private String name;
+    private String Description;
+    private String award_to;
+    private String award_by;
+
+    public Badge(String name, String description, String award_by, String award_to){
+        this.name = name;
+        this.Description = description;
+        this.award_by = award_by;
+        this.award_to = award_to;
+    }
+
+    public String getAward_by() {
+        return award_by;
+    }
+
+    public String getAward_to() {
+        return award_to;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
